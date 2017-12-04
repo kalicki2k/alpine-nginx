ae8b04f3a9067783fe9a9e283e86bd84c8565f6e
Sebastian Kalicki <sebastian@kalicki.email>
2017-12-05 00:43:14 +0100

    Change log folder.

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -4,6 +4,16 @@
 # Version: 1.0
 #
 
+NGINX_LOGS=/var/www/localhost/logs
+
+#
+# Create nginx logs folder
+#
+if [[ ! -d ${NGINX_LOGS} ]]
+then
+    mkdir ${NGINX_LOGS}
+fi
+
 #
 # Starting nginx daemon...
 #
