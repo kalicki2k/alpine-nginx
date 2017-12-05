3cedfa2b631fa782c74f118a3ac1561ea4657198
Sebastian Kalicki <sebastian@kalicki.email>
2017-12-05 01:29:44 +0100

    Remove nginx logs set up...

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -4,14 +4,51 @@
 # Version: 1.0
 #
 
+CONFIG_PATH=/etc/nginx/nginx.conf
+DOCUMENT_ROOT=/var/www/localhost/htdocs
 NGINX_LOGS=/var/www/localhost/logs
+VHOST_PATH=/etc/nginx/conf.d/localhost.conf
 
 #
-# Create nginx logs folder
+# Create nginx logs folder if not exists
 #
-if [[ ! -d ${NGINX_LOGS} ]]
+#if [[ ! -d ${NGINX_LOGS} ]]
+#then
+#    mkdir ${NGINX_LOGS}
+#fi
+
+#
+# Set server name
+#
+if [[ ! -z ${NGINX_SERVER_NAME} ]]
 then
-    mkdir ${NGINX_LOGS}
+    sed -i "s/server_name localhost;/server_name localhost ${NGINX_SERVER_NAME};/" ${VHOST_PATH}
+fi
+
+#
+# Create user and group
+#
+if [[ ! -z ${NGINX_RUN_USER} ]]
+then
+
+    if [[ -z ${NGINX_RUN_GROUP} ]]
+    then
+        NGINX_RUN_GROUP=nginx
+    fi
+
+    sed -i "s/user nginx;/user ${NGINX_RUN_USER} ${NGINX_RUN_GROUP};/" ${CONFIG_PATH}
+
+    if [[ ! -z ${NGINX_RUN_USER_ID} ]] && [[ ! -z ${NGINX_RUN_GROUP_ID} ]]
+    then
+        addgroup -g ${NGINX_RUN_GROUP_ID} ${NGINX_RUN_GROUP}
+        adduser -u ${NGINX_RUN_USER_ID} -G ${NGINX_RUN_GROUP} -h ${DOCUMENT_ROOT} ${NGINX_RUN_USER}
+
+    else
+        addgroup ${NGINX_RUN_GROUP}
+        adduser -G ${NGINX_RUN_GROUP} -h ${DOCUMENT_ROOT} ${NGINX_RUN_USER}
+    fi
+
+    chown -R ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DOCUMENT_ROOT}
 fi
 
 #
